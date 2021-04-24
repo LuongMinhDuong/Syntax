694228af373b411f5207b6ea81d19a935f057574
luongminhduong <Duong40541@gmail.com>
2021-04-24 11:01:32 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,18 @@
+package basic.dev;
+/**
+ * Lab04
+ * @author Admin
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int number;
+		String nameSchool;
+
+	}
+	public void show()	{
+		
+	}
+}
